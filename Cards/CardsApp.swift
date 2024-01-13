dbd612d22733a3be95017024ce791518cf5ea5fa
Ad4mWest <https://github.com/Ad4mWest>
2024-01-13 09:22:59 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Adam West on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
