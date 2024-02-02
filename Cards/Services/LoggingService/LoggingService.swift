6d825ddfe3bb69161e8c7d42de7b472eefe7ed48
Ad4mWest <https://github.com/Ad4mWest>
2024-02-02 15:36:50 +0300

    Added loggings service for tests.

---
+++ Cards/Services/LoggingService/LoggingService.swift
@@ -0,0 +1,8 @@
+//
+//  LoggingService.swift
+//  Cards
+//
+//  Created by Adam West on 02.02.2024.
+//
+
+import Foundation
---
+++ CardsTests/MockLoggingService.swift
@@ -0,0 +1,8 @@
+//
+//  MockLoggingService.swift
+//  CardsTests
+//
+//  Created by Adam West on 02.02.2024.
+//
+
+import Foundation
